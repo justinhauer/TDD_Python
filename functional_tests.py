ce13aa1f7595e1ccb7c9caeb73b8883beee08db6
Justin Hauer <justinhauer@Justins-Mac-mini.local>
2019-05-18 15:23:36 -0500

    "first commit broski"

---
+++ functional_tests.py
@@ -0,0 +1,14 @@
+# from selenium import webdriver
+
+# browser = webdriver.Firefox()
+# browser.get('http://localhost:8000')
+
+# assert 'Django' in browser.title
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
+
