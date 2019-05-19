71764b0f956a9f979dad4c337a9ff6cb835c42b3
Justin Hauer <justinhauer@Justins-Mac-mini.local>
2019-05-19 11:28:46 -0500

    refactor home page view

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -20,10 +22,23 @@ class NewVisitorTest(unittest.TestCase):
 
 #she notices the page title and header mention todo in the lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
+
+
 
 #she is invited to enter a todo item straight away
 
