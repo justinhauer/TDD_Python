25811181a0d1937a417555095088dbd6591059ce
Justin Hauer <justinhauer@gmail.com>
2019-05-19 18:06:52 -0500

    oh another commit

--- functional_tests.py
+++ functional_tests.py
@@ -11,24 +11,29 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
     
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-#edith has heard about a cool new online todo app 
-#she goes to checkout its homepage
-
-
-#edith has heard about a cool new online todo app 
-#she goes to checkout its homepage
-
 #she notices the page title and header mention todo in the lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         inputbox = self.browser.find_element_by_id('id_new_item')
         # self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         #self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), f"New to-do item did not appear in table. Contents were:\n{table.text}")
