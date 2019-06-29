a3dd9281a2956bce2df33ad4e1d333595fb4b576
Justin Hauer <justinhauer@gmail.com>
2019-06-29 13:51:14 -0500

    adding untracked files, on page 243

--- lists/views.py
+++ lists/views.py
@@ -14,7 +14,7 @@ def new_list(request):
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
+    return redirect(f'/lists/{list_.id}/')
 
 
 def view_list(request, list_id):
